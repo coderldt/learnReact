fc4ca6ae60a667e154ac8b6856affdcf6d00c2ef
lvchao <chao.lv@brgroup.com>
2021-10-13 18:06:14 +0800

    feat: 数据

---
+++ src/data/index.js
@@ -0,0 +1,146 @@
+const projectsModulesData = {
+    "rootFile": {
+        "id": "793c7efd-820b-4e36-93aa-6399091278c8",
+        "name": "模型列表",
+        "fullPath": "/",
+        "type": "root",
+        "lock": false,
+        "children": [
+            {
+                "id": "cab17ece-3b3f-4818-90fc-33d4fa417ddf",
+                "name": "闪电贷V1",
+                "fullPath": "/闪电贷V1",
+                "type": "project",
+                "lock": false,
+                "children": [
+                    {
+                        "id": "4edb8f68-fd60-4c4e-abb1-bea7a7059491",
+                        "name": "策略包",
+                        "fullPath": "/闪电贷V1",
+                        "type": "resourcePackage",
+                        "lock": false
+                    },
+                    {
+                        "id": "68cf5632-f847-4cf5-b71a-6dcf0af2c97d",
+                        "name": "资源",
+                        "fullPath": "/闪电贷V1",
+                        "type": "resource",
+                        "lock": false,
+                        "children": [
+                            {
+                                "id": "dc63355a-989c-4fe0-b968-f522e289cdeb",
+                                "name": "库",
+                                "fullPath": "/闪电贷V1",
+                                "type": "lib",
+                                "lock": false,
+                                "children": [
+                                    {
+                                        "id": "331985fc-465b-4d7d-8b29-9685eafff54e",
+                                        "name": "初始化变量库.vl.xml",
+                                        "fullPath": "/闪电贷V1/初始化变量库.vl.xml",
+                                        "type": "variable",
+                                        "lock": false,
+                                        "nodeId": "f1b70a76-aacd-457c-b3dc-7e74d644477c"
+                                    },
+                                    {
+                                        "id": "18405d9b-b457-4b21-a19f-a984edec7836",
+                                        "name": "常量库.cl.xml",
+                                        "fullPath": "/闪电贷V1/常量库.cl.xml",
+                                        "type": "constant",
+                                        "lock": false,
+                                        "nodeId": "26ab90fe-907e-4125-964c-b93c5d09d55e"
+                                    },
+                                    {
+                                        "id": "1b3875a6-cf51-47ae-8c63-ada6dc171fc5",
+                                        "name": "参数库.pl.xml",
+                                        "fullPath": "/闪电贷V1/参数库.pl.xml",
+                                        "type": "parameter",
+                                        "lock": false,
+                                        "nodeId": "403c94a2-e442-43be-af1e-f112d2a952a2"
+                                    }
+                                ]
+                            },
+                            {
+                                "id": "0a104728-8541-47be-a964-b6e91ff9e52e",
+                                "name": "决策集",
+                                "fullPath": "/闪电贷V1",
+                                "type": "ruleLib",
+                                "lock": false,
+                                "children": [
+                                    {
+                                        "id": "c4e75c13-b971-4c72-b970-8fda75c95c55",
+                                        "name": "首查.rs.xml",
+                                        "fullPath": "/闪电贷V1/首查.rs.xml",
+                                        "type": "rule",
+                                        "lock": false,
+                                        "nodeId": "439573bb-beef-4beb-bc26-155c0defa935"
+                                    }
+                                ]
+                            },
+                            {
+                                "id": "d695cb4e-f3a7-47ce-81ed-e00e169a9505",
+                                "name": "决策表",
+                                "fullPath": "/闪电贷V1",
+                                "type": "decisionTableLib",
+                                "lock": false,
+                                "children": [
+                                    {
+                                        "id": "b0afaeec-074e-4159-a166-bc8fea29e42e",
+                                        "name": "决策表.dt.xml",
+                                        "fullPath": "/闪电贷V1/决策表.dt.xml",
+                                        "type": "decisionTable",
+                                        "lock": false,
+                                        "nodeId": "102f1c2e-6456-48db-8a72-98701d257a0f"
+                                    }
+                                ]
+                            },
+                            {
+                                "id": "6e1057f6-8cd9-4129-8efc-045dc1e2780b",
+                                "name": "决策树",
+                                "fullPath": "/闪电贷V1",
+                                "type": "decisionTreeLib",
+                                "lock": false
+                            },
+                            {
+                                "id": "c9b012f7-3f46-46bf-9883-7a1b2b3dd79b",
+                                "name": "评分卡",
+                                "fullPath": "/闪电贷V1",
+                                "type": "scorecardLib",
+                                "lock": false,
+                                "children": [
+                                    {
+                                        "id": "ed143555-5075-4360-b164-5ada8566cd76",
+                                        "name": "评分.sc",
+                                        "fullPath": "/闪电贷V1/评分.sc",
+                                        "type": "scorecard",
+                                        "lock": false,
+                                        "nodeId": "602ec33b-33d5-4f8c-a5a2-0e7b59d5414d"
+                                    }
+                                ]
+                            },
+                            {
+                                "id": "7c430e00-8b72-42a3-b5e9-3f2da774636a",
+                                "name": "决策流",
+                                "fullPath": "/闪电贷V1",
+                                "type": "flowLib",
+                                "lock": false,
+                                "children": [
+                                    {
+                                        "id": "dc93a0ac-0a52-4196-9d56-ef4d6df267af",
+                                        "name": "新.rl.xml",
+                                        "fullPath": "/闪电贷V1/新.rl.xml",
+                                        "type": "flow",
+                                        "lock": false,
+                                        "nodeId": "0a939a3d-6dbd-4f26-865d-9299112a07d1"
+                                    }
+                                ]
+                            }
+                        ]
+                    }
+                ],
+                "nodeId": "2286982e-6142-4287-8fb9-08d757a99de3"
+            }
+        ]
+    },
+    "projectNames": []
+}
